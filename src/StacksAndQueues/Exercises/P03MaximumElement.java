6d3223aeae5123002e0feba5980b4bf3b3af2fe5
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-05-22 10:49:42 +0300

    updated Problem 3

--- src/StacksAndQueues/Exercises/P03MaximumElement.java
+++ src/StacksAndQueues/Exercises/P03MaximumElement.java
@@ -1,11 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class P03MaximumElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            String[] line = scanner.nextLine().split("\\s+");
+            String command = line[0];
 
+            switch (command) {
+                case "1":
+                    int x = Integer.parseInt(line[1]);
+                    stack.push(x);
+                    break;
+                case "2":
+                        stack.pop();
+                    break;
+                case "3":
+                    System.out.println(Collections.max(stack));
+                    break;
+            }
+        }
     }
 
 }
