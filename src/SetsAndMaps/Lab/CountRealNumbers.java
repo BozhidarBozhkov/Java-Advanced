eae9bf678d63d0c3cd6e1e14116a0d7c0aaf150a
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-05-31 21:07:39 +0300

    Sets and Maps lab

--- src/SetsAndMaps/Lab/CountRealNumbers.java
+++ src/SetsAndMaps/Lab/CountRealNumbers.java
@@ -1,3 +1,30 @@
-package SetsAndMaps.Lab;public class CountRealNumbers {
+package SetsAndMaps.Lab;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+
+        Map<Double, Integer> mapNumber = new LinkedHashMap<>();
+
+        for (double n: numbers) {
+            if (!mapNumber.containsKey(n)) {
+                mapNumber.put(n, 1);
+            } else {
+                mapNumber.put(n, mapNumber.get(n) + 1);
+            }
+        }
+        for (double key: mapNumber.keySet()) {
+            System.out.printf("%.1f -> %d%n", key, mapNumber.get(key));
+        }
+    }
 
 }
