df30818f5c67ca6e737924814f90f7d7198e45d6
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-06-04 19:18:22 +0300

    initial commit

---
+++ src/StreamsFilesAndDirectories/Lab/ReadFile.java
@@ -0,0 +1,24 @@
+package StreamsFilesAndDirectories.Lab;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        String path = "D:\\SoftUni\\Java-Advanced\\src\\StreamsFilesDirectories\\input.txt";
+
+        try  {
+            FileInputStream fileStream = new FileInputStream(path);
+            int oneByte = fileStream.read();
+            while (oneByte != -1) {
+                System.out.print(Integer.toBinaryString(oneByte) + " ");
+                oneByte = fileStream.read();
+
+            }
+
+        } catch (IOException ignored) {
+        }
+
+    }
+
+}
