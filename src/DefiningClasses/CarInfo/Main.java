3490dd2d75f654a6927ef90118ea86d5e6baf4c5
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-06-08 22:38:49 +0300

    initial commit

---
+++ src/DefiningClasses/CarInfo/Main.java
@@ -0,0 +1,27 @@
+package DefiningClasses.CarInfo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] carData = scanner.nextLine().split(" ");
+            String brand = carData[0];
+            String model = carData[1];
+            int hp = Integer.parseInt(carData[2]);
+
+            Car car = new Car();
+
+            car.setBrand(brand);
+            car.setModel(model);
+            car.setHorsePower(hp);
+
+            System.out.println(car.carInfo());
+        }
+
+    }
+
+}
