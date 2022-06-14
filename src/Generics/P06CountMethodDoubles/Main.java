64e008c1ed4fbe7cf0513a6556a5af92a545ea98
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-06-14 23:08:37 +0300

    Problem 6 -> Generic Count Method Double

---
+++ src/Generics/P06CountMethodDoubles/Main.java
@@ -0,0 +1,21 @@
+package Generics.P06CountMethodDoubles;
+
+import Generics.P05CountMethodString.Box;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<Double> box = new Box<>();
+        while (n-- > 0) {
+            box.add(Double.parseDouble(scanner.nextLine()));
+        }
+        double value = Double.parseDouble(scanner.nextLine());
+        System.out.println(box.countGreater(value));
+    }
+
+}
