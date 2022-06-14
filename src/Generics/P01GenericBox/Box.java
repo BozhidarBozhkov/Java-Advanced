b083f3f49df73ff74cf094c95b10403f7a4b7ec7
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-06-14 21:53:24 +0300

    Problem 1 -> Generic Box

---
+++ src/Generics/P01GenericBox/Box.java
@@ -0,0 +1,15 @@
+package Generics.P01GenericBox;
+
+public class Box<T> {
+    T element;
+
+    public Box (T element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        //java.lang.String: life in a box
+        return String.format("%s: %s%n", element.getClass().getName(), element);
+    }
+}
---
+++ src/Generics/P01GenericBox/Main.java
@@ -0,0 +1,18 @@
+package Generics.P01GenericBox;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (n-- > 0) {
+            Box<String> stringBox = new Box<>(scanner.nextLine());
+            System.out.print(stringBox);
+
+        }
+    }
+
+}
