a733337a8b7307fe3ed8b74e4441b94f12b77b39
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-06-19 20:00:06 +0300

    Problem 8 -> Custom List Sorter

---
+++ src/Generics/P08CustomListSorter/Sorter.java
@@ -0,0 +1,15 @@
+package Generics.P08CustomListSorter;
+
+public class Sorter {
+    public static <E extends Comparable<E>> void sort(Box2<E> elements){
+        for (int i=0; i<elements.size(); i++){
+            E temp = elements.get(i);
+            for (int j = i+1; j< elements.size(); j++){
+                if(temp.compareTo(elements.get(j)) >0 ) {
+                    elements.swap(i, j);
+                }
+            }
+        }
+    }
+
+}
