b0128c4d72b5f304c70010602dcaac4a73103ce4
Bozhidar Bozhkov <bozhidar.bozhkov@outlook.com>
2022-06-20 22:23:23 +0300

    exam task Mouse and Cheese

---
+++ src/ExamPrep/MouseAndCheese.java
@@ -0,0 +1,107 @@
+package ExamPrep;
+
+import java.util.Scanner;
+
+public class MouseAndCheese {
+
+    private static int row = 0;
+    private static int col = 0;
+    private static int eatenCheese = 0;
+    private static boolean mouseIsInFiled = true;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+
+        char[][] field = new char[size][size];
+
+        for (int i = 0; i < size; i++) {
+            String line = scanner.nextLine();
+            field[i] = line.toCharArray();
+
+            if (line.contains("M")) {
+                row = i;
+                col = line.indexOf("M");
+            }
+        }
+
+        String command = scanner.nextLine();
+
+
+        while (!command.equals("end")) {
+
+            if (command.equals("up")) {
+                moveMouse(field, -1, 0);
+            } else if (command.equals("down")) {
+                moveMouse(field, 1, 0);
+            } else if (command.equals("left")) {
+                moveMouse(field, 0, -1);
+            } else if (command.equals("right")) {
+                moveMouse(field, 0, 1);
+            }
+
+            if (!mouseIsInFiled) {
+                break;
+            }
+            command = scanner.nextLine();
+        }
+
+        if (!mouseIsInFiled) {
+            System.out.println("Where is the mouse?");
+        }
+
+        if (eatenCheese >= 5) {
+            System.out.println("Great job, the mouse is fed " + eatenCheese + " cheeses!");
+        } else {
+            System.out.println("The mouse couldn't eat the cheeses, she needed " + (5 - eatenCheese) + " cheeses more.");
+        }
+
+        print2DArray(field);
+    }
+
+    private static void print2DArray(char[][] field) {
+        for (char[] arr : field) {
+            for (char c : arr) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void moveMouse(char[][] field, int rowMutator, int colMutator) {
+
+        int nextRow = row + rowMutator;
+        int nextCol = col + colMutator;
+
+        boolean isBonusHit = false;
+
+        if (!isInBounds(field, nextRow, nextCol)) {
+            field[row][col] = '-';
+            mouseIsInFiled = false;
+            return;
+        }
+
+        if (field[nextRow][nextCol] == 'c') {
+            eatenCheese++;
+        } else if (field[nextRow][nextCol] == 'B') {
+            isBonusHit = true;
+        }
+
+        field[row][col] = '-';
+        field[nextRow][nextCol] = 'M';
+        row = nextRow;
+        col = nextCol;
+
+        if (isBonusHit) {
+            moveMouse(field, rowMutator, colMutator);
+        }
+    }
+
+    private static boolean isInBounds(char[][] field, int row, int c) {
+        return row >= 0 && row < field.length && c >= 0 && c < field[row].length;
+    }
+
+
+}
